029097581b26e073b6d99489bb42f95bebdf9088
USUARIO <USUARIO@DESKTOP-AVM0NMD>
2022-05-03 10:03:34 -0500

    Agregar archivos de proyecto.

---
+++ wLoadFile/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace wLoadFile
+{
+    partial class frmCargarArchivo
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grpCargarArchivo = new System.Windows.Forms.GroupBox();
+            this.txtLineaLinea = new System.Windows.Forms.TextBox();
+            this.dtgCSV = new System.Windows.Forms.DataGridView();
+            this.btnLineaLinea = new System.Windows.Forms.Button();
+            this.btnCSV = new System.Windows.Forms.Button();
+            this.grpCargarArchivo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgCSV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grpCargarArchivo
+            // 
+            this.grpCargarArchivo.Controls.Add(this.btnCSV);
+            this.grpCargarArchivo.Controls.Add(this.btnLineaLinea);
+            this.grpCargarArchivo.Controls.Add(this.dtgCSV);
+            this.grpCargarArchivo.Controls.Add(this.txtLineaLinea);
+            this.grpCargarArchivo.Location = new System.Drawing.Point(31, 35);
+            this.grpCargarArchivo.Name = "grpCargarArchivo";
+            this.grpCargarArchivo.Size = new System.Drawing.Size(733, 379);
+            this.grpCargarArchivo.TabIndex = 0;
+            this.grpCargarArchivo.TabStop = false;
+            this.grpCargarArchivo.Text = "Cargar Archivo";
+            // 
+            // txtLineaLinea
+            // 
+            this.txtLineaLinea.Location = new System.Drawing.Point(27, 51);
+            this.txtLineaLinea.Multiline = true;
+            this.txtLineaLinea.Name = "txtLineaLinea";
+            this.txtLineaLinea.Size = new System.Drawing.Size(263, 234);
+            this.txtLineaLinea.TabIndex = 0;
+            // 
+            // dtgCSV
+            // 
+            this.dtgCSV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgCSV.Location = new System.Drawing.Point(334, 51);
+            this.dtgCSV.Name = "dtgCSV";
+            this.dtgCSV.Size = new System.Drawing.Size(294, 234);
+            this.dtgCSV.TabIndex = 1;
+            // 
+            // btnLineaLinea
+            // 
+            this.btnLineaLinea.Location = new System.Drawing.Point(95, 316);
+            this.btnLineaLinea.Name = "btnLineaLinea";
+            this.btnLineaLinea.Size = new System.Drawing.Size(117, 34);
+            this.btnLineaLinea.TabIndex = 2;
+            this.btnLineaLinea.Text = "Linea a Linea";
+            this.btnLineaLinea.UseVisualStyleBackColor = true;
+            this.btnLineaLinea.Click += new System.EventHandler(this.btnLineaLinea_Click);
+            // 
+            // btnCSV
+            // 
+            this.btnCSV.Location = new System.Drawing.Point(454, 316);
+            this.btnCSV.Name = "btnCSV";
+            this.btnCSV.Size = new System.Drawing.Size(99, 34);
+            this.btnCSV.TabIndex = 3;
+            this.btnCSV.Text = "CSV";
+            this.btnCSV.UseVisualStyleBackColor = true;
+            this.btnCSV.Click += new System.EventHandler(this.btnCSV_Click);
+            // 
+            // frmCargarArchivo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.grpCargarArchivo);
+            this.Name = "frmCargarArchivo";
+            this.Text = "Cargar Archivo";
+            this.grpCargarArchivo.ResumeLayout(false);
+            this.grpCargarArchivo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgCSV)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpCargarArchivo;
+        private System.Windows.Forms.Button btnCSV;
+        private System.Windows.Forms.Button btnLineaLinea;
+        private System.Windows.Forms.DataGridView dtgCSV;
+        private System.Windows.Forms.TextBox txtLineaLinea;
+    }
+}
+
---
+++ wLoadFile/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+/// <summary>
+/// Fecha: Mayo 3
+/// Autor: Juan Manuel Torres
+/// Descripcion: proyecto carga de archivo
+/// </summary>
+
+namespace wLoadFile
+{
+    public partial class frmCargarArchivo : Form
+    {
+        public frmCargarArchivo()
+        {
+            InitializeComponent();
+        }
+
+        private Stream myStream; // clase que permite guardar archivo linea a linea, que se pueda visualizar
+        int counter = 0;
+        string line;
+
+        private void btnLineaLinea_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Application.StartupPath;
+            openFileDialog.Filter = "Archivos (*.txt) | *.txt";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                           System.IO.StreamReader file = new System.IO.StreamReader(openFileDialog.FileName);
+
+                           while((line = file.ReadLine()) != null)
+                           {
+                               txtLineaLinea.Text = txtLineaLinea.Text + line;
+                               counter++;
+                           }
+                           file.Close();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show (ex.Message);
+                }
+            }
+        }
+
+        private void btnCSV_Click(object sender, EventArgs e)
+        {
+            DataGridViewTextBoxColumn col1 = new DataGridViewTextBoxColumn();
+            col1.HeaderText = "ID";
+            col1.Width = 200;
+            col1.ReadOnly = true;
+            dtgCSV.Columns.Add(col1);
+
+            DataGridViewTextBoxColumn col2 = new DataGridViewTextBoxColumn();
+            col2.HeaderText = "NOM";
+            col2.Width = 200;
+            col2.ReadOnly = true;
+            dtgCSV.Columns.Add(col2);
+
+            DataGridViewTextBoxColumn col3 = new DataGridViewTextBoxColumn();
+            col3.HeaderText = "APEL";
+            col3.Width = 200;
+            col3.ReadOnly = true;
+            dtgCSV.Columns.Add(col3);
+
+            char delimitador = ';';
+            string[] valores;
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Application.StartupPath;
+            openFileDialog.Filter = "Archivos (*.CSV) | *.CSV";
+
+            if ((openFileDialog.ShowDialog()) == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog.OpenFile()) != null)
+                    {
+                        System.IO.StreamReader file = new System.IO.StreamReader(myStream);
+
+                        while ((line = file.ReadLine()) != null)
+                        {
+                            if (counter >= 1)
+                            {
+                                valores = line.Split(delimitador);  
+
+                                dtgCSV.Rows.Add(valores.ToArray());
+                                counter++;
+                            }
+                            else
+                            {
+                                counter++;
+                            }
+                        }
+                        file.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
